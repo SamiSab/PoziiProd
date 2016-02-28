040a736d12761897f1a5dd2b9e47664f03a8f1b9
Bhavika Sharma <sharma.bhavika@hotmail.com>
2016-02-28 11:52:14 -0500

    1 entry working

--- app/src/main/java/com/example/analia/apptest/Gratitude.java
+++ app/src/main/java/com/example/analia/apptest/Gratitude.java
@@ -23,7 +23,7 @@ import org.w3c.dom.Text;
 import java.util.ArrayList;
 
 public class Gratitude extends AppCompatActivity {
-
+//pls
 public static String [] text;
     public EditText input;
     public TextView output;
